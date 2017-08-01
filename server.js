3bff0e7e27a9b1a0aac910fa847ad434764b81e5
katawut.c <katawut.c@gmail.com>
2017-08-01 16:23:51 +0700

    connect to MLab with mongoose

--- server.js
+++ server.js
@@ -12,6 +12,25 @@ var app = express();
 // set static folder to render the page
 app.use(express.static('app'));
 
+/** mongoose */
+var mongoose = require('mongoose');
+
+var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+                replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };
+
+var localDBuri = 'mongodb://localhost:27017/MEP';
+var mlabDBuri = 'mongodb://katawut:1!MongoMongoose@ds129023.mlab.com:29023/mep';
+
+
+mongoose.connect(mlabDBuri, options);
+//mongoose.connect(localDBuri);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('mongoose mongodb <1!MongoMongoose> connected...');
+});
+/** */
+
 app.listen(port, function(){
 	console.log('Server starts on port '+ port);
 });
