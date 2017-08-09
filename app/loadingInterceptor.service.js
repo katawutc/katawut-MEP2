71be7f05bc698bda84bb4ada3d12fdb33d0e3f5d
katawut.c <katawut.c@gmail.com>
2017-08-09 09:05:30 +0700

    add loading logic; but still need yo apply spinner into

---
+++ app/loadingInterceptor.service.js
@@ -0,0 +1,43 @@
+angular.module('app').factory('loadingInterceptorService', loadingInterceptorService);
+
+function loadingInterceptorService($q, $rootScope, $log) {
+  var xhrCreations = 0;
+ var xhrResolutions = 0;
+
+ function isLoading() {
+     return xhrResolutions < xhrCreations;
+ }
+
+ function updateStatus() {
+     $rootScope.loading = isLoading();
+ }
+
+ return {
+     request: function (config) {
+         xhrCreations++;
+         updateStatus();
+         return config;
+     },
+     requestError: function (rejection) {
+         xhrResolutions++;
+         updateStatus();
+         $log.error('Request error:', rejection);
+         return $q.reject(rejection);
+     },
+     response: function (response) {
+     xhrResolutions++;
+     updateStatus();
+     return response;
+ },
+ responseError: function (rejection) {
+     xhrResolutions++;
+     updateStatus();
+     $log.error('Response error:', rejection);
+     return $q.reject(rejection);
+   }
+ }
+}
+
+angular.module('app').config(['$httpProvider', function($httpProvider) {
+    $httpProvider.interceptors.push('loadingInterceptorService');
+}]);
