058c27e02a4603c9b2b0fd9b6e5f19af0310203e
katawut.c <katawut.c@gmail.com>
2017-09-12 11:53:15 +0700

    get the log in status; still need to implement the asynchronous to route to the log in and dasgboard page

--- app/navBar.controller.js
+++ app/navBar.controller.js
@@ -1,6 +1,7 @@
 angular.module('app').controller('navBarController', navBarController);
 
 function navBarController ($scope, $http, $location, $window) {
+
     $scope.logIn = false;
 
     $scope.isLogIn = function() {
@@ -10,4 +11,32 @@ function navBarController ($scope, $http, $location, $window) {
     if ($window.sessionStorage.getItem('logInmessage') === 'login success') {
       $scope.logIn = true;
     } else { $scope.logIn = false; }
+
+    $scope.checkFBLogIn = function() {
+      console.log('checkFBLogIn');
+
+      if($window.FB) console.log('$window.FB');
+
+      console.log($window.FB);
+
+
+      $window.FB.init({
+        appId      : '141198316480017',
+        status     : true,
+        cookie     : true,  // enable cookies to allow the server to access
+                        // the session
+        xfbml      : true,  // parse social plugins on this page
+        version    : 'v2.8' // use graph api version 2.8
+      })
+
+      $window.FB.getLoginStatus(function(response) {
+        console.log('getLoginStatus');
+        console.log(response.status);
+        //statusChangeCallback(response);
+        if (response.status === 'unknown') {
+          $location.path('/logIn');
+        }
+      });
+
+    }
 }
