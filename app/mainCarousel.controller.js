05ac329e2ed23577d7c7912768ae6f846b6e7fdc
katawut.c <katawut.c@gmail.com>
2017-09-13 14:59:35 +0700

    carousel works; still need to find or adjust the right image size

--- app/mainCarousel.controller.js
+++ app/mainCarousel.controller.js
@@ -3,15 +3,30 @@ angular.module('app').controller('mainCarouselCtrl',
 
 function mainCarouselCtrl ($scope) {
 
-    $scope.myInterval = 1000;
+    $scope.myInterval = 1500;
     $scope.noWrapSlides = false;
     $scope.active = 0;
     var slides = $scope.slides = [];
     var currIndex = 0;
 
     slides.push({
+      image: 'image/mep01.jpg',
+      id: currIndex++
+    });
+
+    slides.push({
       image: 'image/mep02.jpg',
-         id: currIndex++
-});
+      id: currIndex++
+    });
+
+    slides.push({
+      image: 'image/mep03.jpg',
+      id: currIndex++
+    });
+
+    slides.push({
+      image: 'image/mep04.jpg',
+      id: currIndex++
+    });
 
   }
