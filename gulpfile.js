849ef28d67c377816b012169221a921a847ac66a
katawut.c <katawut.c@gmail.com>
2017-09-14 12:09:53 +0700

    1st implement gulpfile.js; still cannot link the minify js

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+//var imagemin = require('gulp-imagemin');
+
+var path = {
+  script: 'app/*.js',
+  image: 'public/image/*.jpg',
+  css: 'public/css/*.css'
+};
+
+/**
+ * 1. Lint our JavaScript. (Seriously. Do it.)
+ * 2. Compile our Sass files. (Browsers can’t read that stuff...)
+ * 3. Concatenate our JavaScript. (Reduce HTTP Requests!)
+ * 4. Minify and rename concatenated files. (Every little bit counts!)
+ */
+
+ // Lint Task
+gulp.task('lint', function() {
+    return gulp.src('app/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+// Concatenate & Minify JS
+gulp.task('script', function() {
+    return gulp.src('app/*.js')
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(rename('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch('app/*.js', ['lint', 'script']);
+});
+
+gulp.task('default',  ['lint', 'script', 'watch']);
