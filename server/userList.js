65e431fcdd60aa38a8cdd2077011d0dd8b9c90ae
katawut.c <katawut.c@gmail.com>
2017-09-18 16:01:05 +0700

    implement the ad authorization logic

--- server/userList.js
+++ server/userList.js
@@ -4,17 +4,24 @@ module.exports = function userList(req, res) {
   var mongo = require('./mongoDBConnect');
   var db = mongo.getDB();
 
-  console.log(req.user);
+  // to put the authority checking here
 
-  if (req.user.userRole === 'ad') {
-    db.collection('user').find({}, {userID:1, userName:1, userRole:1, _id:0}).toArray(function(err, doc) {
-      if (err) throw err;
-      // to send a list of userID, userName, userRole
-      else {
-        console.log(doc);
-        res.json(doc);
-      }
-    })
+  if (req.user.userRole !== 'ad' ||
+        req.user.userID !== req.params.userID) {
+
+          res.json({errorMessage: 'no authority'});
+        }
+  else if (req.user.userRole === 'ad' &&
+        req.user.userID === req.params.userID) {
+
+          db.collection('user').find({}, {userID:1, userName:1, userRole:1, _id:0}).toArray(function(err, doc) {
+            if (err) throw err;
+            // to send a list of userID, userName, userRole
+            else {
+              console.log(doc);
+              res.json(doc);
+            }
+          })
   }
   else {
     res.json({errorMessage: 'no authority'});
