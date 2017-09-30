7cbae929a91b6c7f3877aac6502b781c8c3b4741
katawut.c <katawut.c@gmail.com>
2017-09-30 16:38:37 +0700

    test chat and note panels toggle

--- app/secondNavBar.su.controller.js
+++ app/secondNavBar.su.controller.js
@@ -3,11 +3,28 @@ angular.module('app').controller('secondNavBarSuCtrl',
 
 function secondNavBarSuCtrl($scope) {
 
+  $scope.showNotePanel = false;
+  $scope.showChatPanel = false;
+
   $scope.openChatPanel = function() {
-    console.log('open chat panel');
+
+    $scope.showChatPanel = !$scope.showChatPanel;
+    if ($scope.showChatPanel) {
+      console.log('open chat panel');
+    }
+    else if (!$scope.showChatPanel) {
+      console.log('close chat panel');
+    }
   }
 
   $scope.openNotePanel = function() {
-        console.log('open note panel');
+
+    $scope.showNotePanel = !$scope.showNotePanel;
+    if ($scope.showNotePanel) {
+      console.log('open note panel');
+    }
+    else if (!$scope.showNotePanel) {
+      console.log('close note panel');
+    }
   }
 }
