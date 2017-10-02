e0abc270c8af7a644000853ccd74e50de0122022
katawut.c <katawut.c@gmail.com>
2017-10-02 10:23:06 +0700

    create su test exam mode empty answer sheet

--- server/createSuExamSheet.js
+++ server/createSuExamSheet.js
@@ -7,6 +7,24 @@ module.exports = function createSuExamSheet(req, res) {
   console.log(req.params);
 
 
+  for(var i = 1; i <= req.body.suTestSize; i++) {
+
+    var suTestQuestionNumber = i.toString();
+
+    db.collection('suTestHistory').insert({userID: req.body.userID,
+                                            suTestID: req.body.suTestID,
+                                            suTestMode: req.body.suTestMode,
+                                            suTestStartAt: req.body.suTestStartAt,
+                                            suTestQuestionNumber: suTestQuestionNumber}, cb);
+       function cb(err, doc) {
+         if (err) throw err;
+         else {
+           console.log('empty answer sheet is created for '+suTestQuestionNumber);
+         }
+       }
+  }
+
+
   res.json('return from createSuExamSheet');
 
 }
