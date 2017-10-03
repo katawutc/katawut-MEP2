3622f351607cb50a8d010a81e6bd5f85994c4131
katawut.c <katawut.c@gmail.com>
2017-10-03 09:24:51 +0700

    get the suTest exam question to revise

--- app/reviseSuTestExamAnswerSheet.controller.js
+++ app/reviseSuTestExamAnswerSheet.controller.js
@@ -1,18 +1,33 @@
 angular.module('app').controller('reviseSuTestExamAnswerSheetCtrl',
-                                  ['$scope', '$http', '$routeParams',
+                                  ['$scope', '$http', '$route',
                                     '$window', '$location',
                                       'currentUserAnswer', 'currentExamQuestion',
-                                      reviseSuTestExamAnswerSheetCtrl]);
+                                        reviseSuTestExamAnswerSheetCtrl]);
 
-function reviseSuTestExamAnswerSheetCtrl($scope, $http, $routeParams,
+function reviseSuTestExamAnswerSheetCtrl($scope, $http, $route,
                                           $window, $location,
                                           currentUserAnswer, currentExamQuestion) {
+
+  $scope.suTestID = $window.sessionStorage.suTestID;
 
   console.log('at reviseSuTestExamAnswerSheetCtrl');
 
   console.log(currentUserAnswer);
 
   console.log(currentExamQuestion);
+
+  /** handle currentExamQuestion */
+  if (currentExamQuestion) {
+
+    $scope.suTestQuestionNumber = $route.current.params.suTestQuestionNumber;
+    $scope.suTestSize = $window.sessionStorage.suTestSize;
+    $scope.question = currentExamQuestion.question;
+    $scope.choice1 = currentExamQuestion.answerChoice[0];
+    $scope.choice2 = currentExamQuestion.answerChoice[1];
+    $scope.choice3 = currentExamQuestion.answerChoice[2];
+    $scope.choice4 = currentExamQuestion.answerChoice[3];
+
+  }
 
 
 
--- server/getSuTestExamQuestion.js
+++ server/getSuTestExamQuestion.js
@@ -2,10 +2,15 @@ module.exports = function getSuTestExamQuestion(req, res) {
 
   var mongo = require('./mongoDBConnect');
   var db = mongo.getDB();
+  
+  // the implementation duplicate with getSuTestQuestion
+  var questionArrayIndex = req.params.suTestQuestionNumber - 1;
 
-  console.log('at server getSuTestExamQuestion');
-
-
-  res.json('return from getSuTestExamQuestion');
-
+  db.collection('suNewTest').findOne({userID: req.params.userID,
+                                      suTestID: req.params.suTestID}, function(err, doc) {
+    if (err) throw err;
+    if (doc && doc.suTest) {
+      res.json(doc.suTest[questionArrayIndex]);
+    }
+  })
 }
