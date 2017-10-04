6cd56b98fa3838232e3398ed08b83f553cf794fb
katawut.c <katawut.c@gmail.com>
2017-10-04 13:34:30 +0700

    update

--- server/generateSuNewTest.js
+++ server/generateSuNewTest.js
@@ -5,6 +5,35 @@ module.exports = function generateSuNewTest(req, res) {
 
   console.log('at server: generateSuNewTest');
 
+  /**
+   * 1. check setting
+   * 2. check suNewTestService; testStatus is in here
+   * 3. check su new test status e.g. registered?
+   * 4. provide suTestID
+   */
+
+   if (req.mepSuAccess !== true) {
+     res.json({errorMessage: 'no authority'});
+   }
+
+   if (req.mepSuAccess === true) {
+     // 1. check setting
+     db.collection('userSetting').findOne({userID: req.params.userID,
+                                           userRole: 'su'}, cb);
+
+     function cb(err, setting) {
+       if (err) throw err;
+       if (setting) {
+            console.log(setting);
+            // create testID here
+            // check testID in suNewTestService DB
+            // check suTestID number
+            // generate suTestID
+          }
+       }
+    }
+
+
 
 
   res.json('su new test is generated from server');
