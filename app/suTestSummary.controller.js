fa673b4d058797e0112e7db7e6c4d33a333748c1
katawut.c <katawut.c@gmail.com>
2017-10-04 11:10:49 +0700

    update

--- app/suTestSummary.controller.js
+++ app/suTestSummary.controller.js
@@ -7,10 +7,14 @@ angular.module('app').controller('suTestSummaryCtrl',
 function suTestSummaryCtrl($scope, $http, $routeParams,
                           $window, $location, suTestSummary, suTestScore) {
 
-  $scope.suTestID = suTestSummary[0].suTestID;
-  $scope.suTestMode = suTestSummary[0].suTestMode;
-  var suTestStartAt = parseInt(suTestSummary[0].suTestStartAt);
-  $scope.suTestStartAt = (new Date(suTestStartAt)).toString();
+  console.log(suTestSummary);
+
+  if(suTestSummary && suTestSummary[0]) {
+    $scope.suTestID = suTestSummary[0].suTestID;
+    $scope.suTestMode = suTestSummary[0].suTestMode;
+    var suTestStartAt = parseInt(suTestSummary[0].suTestStartAt);
+    $scope.suTestStartAt = (new Date(suTestStartAt)).toString();
+  }
 
   if(suTestSummary) {
     $scope.result = suTestSummary;
