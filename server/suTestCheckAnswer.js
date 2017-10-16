8bfb008ad0d386849b0957f4bc6075996b03feae
katawut.c <katawut.c@gmail.com>
2017-10-16 13:30:11 +0700

    refactor callback functions

--- server/suTestCheckAnswer.js
+++ server/suTestCheckAnswer.js
@@ -3,66 +3,71 @@ module.exports = function suTestCheckAnswer(req, res) {
   var mongo = require('./mongoDBConnect');
   var db = mongo.getDB();
 
-  db.collection('suSolutionContent').findOne({solutionID: req.body.testID,
-                                              solQuestionNumber: req.body.questionNumber},
-    function(err, doc) {
-      if (err) throw err;
-      if (doc) {
-        if (req.body.answer === doc.solution) {
+  function foundSuSolution_cb(err, doc) {
 
-          //suTestHistory
-          db.collection('suTestAnswerSheet')
-          .update({userID: req.body.userID,
-                   testID: req.body.testID,
-                   suTestNumber: req.body.suTestNumber,
-                   suTestID: req.body.suTestID,
-                   suTestMode: req.body.suTestMode,
-                   suTestStartAt: req.body.suTestStartAt,
-                   suTestQuestionNumber: req.body.suTestQuestionNumber},
-                   { $set:
+    if (err) throw err;
+    if (doc) {
+      if (req.body.answer === doc.solution) {
+
+        //suTestHistory
+        db.collection('suTestAnswerSheet')
+        .update({userID: req.body.userID,
+                 testID: req.body.testID,
+                 suTestNumber: req.body.suTestNumber,
+                 suTestID: req.body.suTestID,
+                 suTestMode: req.body.suTestMode,
+                 suTestStartAt: req.body.suTestStartAt,
+                 suTestQuestionNumber: req.body.suTestQuestionNumber},
+                 { $set:
+                    {
+                      suTestAnswer: req.body.answer,
+                      suTestResult: 'correct'
+                    }
+                 }, cb);
+
+        function cb(err, doc) {
+          if (err) throw err;
+          if (doc) {
+            res.json({result: 'correct',
+                      explanation: doc.explanation});
+          }
+        }
+      }
+      else if (req.body.answer !== doc.solution) {
+
+        //suTestHistory
+        db.collection('suTestAnswerSheet')
+        .update({userID: req.body.userID,
+                 testID: req.body.testID,
+                 suTestNumber: req.body.suTestNumber,
+                 suTestID: req.body.suTestID,
+                 suTestMode: req.body.suTestMode,
+                 suTestStartAt: req.body.suTestStartAt,
+                 suTestQuestionNumber: req.body.suTestQuestionNumber},
+                    {$set:
                       {
                         suTestAnswer: req.body.answer,
-                        suTestResult: 'correct'
+                        suTestResult: 'wrong'
                       }
-                   }, cb);
+                    }, cb);
 
-          function cb(err, doc) {
-            if (err) throw err;
-            if (doc) {
-              res.json({result: 'correct',
-                        explanation: doc.explanation});
-            }
+        function cb(err, doc) {
+          if (err) throw err;
+          if (doc) {
+            res.json({result: 'wrong',
+                      explanation: doc.explanation});
           }
         }
-        else if (req.body.answer !== doc.solution) {
-
-          //suTestHistory
-          db.collection('suTestAnswerSheet')
-          .update({userID: req.body.userID,
-                   testID: req.body.testID,
-                   suTestNumber: req.body.suTestNumber,
-                   suTestID: req.body.suTestID,
-                   suTestMode: req.body.suTestMode,
-                   suTestStartAt: req.body.suTestStartAt,
-                   suTestQuestionNumber: req.body.suTestQuestionNumber},
-                      {$set:
-                        {
-                          suTestAnswer: req.body.answer,
-                          suTestResult: 'wrong'
-                        }
-                      }, cb);
-
-          function cb(err, doc) {
-            if (err) throw err;
-            if (doc) {
-              res.json({result: 'wrong',
-                        explanation: doc.explanation});
-            }
-          }
-        }
-      else {
-          res.json({errorMessage: 'error'});
       }
+    else {
+        res.json({errorMessage: 'error'});
     }
-  })
+  }
+}
+
+  // main entry point of the module */
+  db.collection('suSolutionContent')
+  .findOne({solutionID: req.body.testID,
+            solQuestionNumber: req.body.questionNumber},
+            foundSuSolution_cb);
 }
--- server/submitAnswerSuTestExamMode.js
+++ server/submitAnswerSuTestExamMode.js
@@ -4,7 +4,57 @@ module.exports = function submitAnswerSuTestExamMode(req, res) {
   var mongo = require('./mongoDBConnect');
   var db = mongo.getDB();
 
-  //suTestHistory
+
+  function updateSuTestAnswerSheet_cb(err, doc) {
+    if (err) throw err;
+    else {
+      // checking answer and update the score here
+      var solutionID = req.body.testID;
+      var questionNumber = req.body.questionNumber;
+      var solution;
+      var userAnswer = req.body.suTestAnswer;
+
+      // Retrieve Solution from the DB
+      db.collection('suSolutionContent').findOne({solutionID: solutionID,
+                                                  solQuestionNumber: questionNumber},
+                                                  cb); /*function (err, doc) { */
+      function cb(err, solDoc) {
+
+        if (err) throw err;
+        else {
+          solution = solDoc.solution;
+
+          if (solution === userAnswer) {
+
+            db.collection('suTestAnswerSheet')
+            .update({userID: req.body.userID,
+                     testID: req.body.testID,
+                     suTestNumber: req.body.suTestNumber,
+                     suTestID: req.body.suTestID,
+                     suTestMode: req.body.suTestMode,
+                     suTestStartAt: req.body.suTestStartAt,
+                     suTestQuestionNumber: req.body.suTestQuestionNumber},
+                        { $set: {suTestResult: 'correct'}});
+          }
+          else {
+            //suTestHistory
+            db.collection('suTestAnswerSheet')
+            .update({userID: req.body.userID,
+                     testID: req.body.testID,
+                     suTestNumber: req.body.suTestNumber,
+                     suTestID: req.body.suTestID,
+                     suTestMode: req.body.suTestMode,
+                     suTestStartAt: req.body.suTestStartAt,
+                     suTestQuestionNumber: req.body.suTestQuestionNumber},
+                        { $set: {suTestResult: 'wrong'}});
+          }
+        }
+      }
+      res.json('answer recorded');
+    }
+  }
+
+  /** main module entry */
   db.collection('suTestAnswerSheet')
   .update({userID: req.body.userID,
            testID: req.body.testID,
@@ -17,55 +67,6 @@ module.exports = function submitAnswerSuTestExamMode(req, res) {
             { suTestQuestionStatus: req.body.suTestQuestionStatus,
               suTestAnswer: req.body.suTestAnswer,
               suTestQuestionStatus: 'answered'}
-          }, cb);
+          }, updateSuTestAnswerSheet_cb);
 
-    function cb (err, doc) {
-      if (err) throw err;
-      else {
-        // checking answer and update the score here
-        var solutionID = req.body.testID;
-        var questionNumber = req.body.questionNumber;
-        var solution;
-        var userAnswer = req.body.suTestAnswer;
-
-        // Retrieve Solution from the DB
-        db.collection('suSolutionContent').findOne({solutionID: solutionID,
-                                                    solQuestionNumber: questionNumber},
-                                                    cb); /*function (err, doc) { */
-        function cb(err, solDoc) {
-
-          if (err) throw err;
-          else {
-            solution = solDoc.solution;
-
-            if (solution === userAnswer) {
-
-              db.collection('suTestAnswerSheet')
-              .update({userID: req.body.userID,
-                       testID: req.body.testID,
-                       suTestNumber: req.body.suTestNumber,
-                       suTestID: req.body.suTestID,
-                       suTestMode: req.body.suTestMode,
-                       suTestStartAt: req.body.suTestStartAt,
-                       suTestQuestionNumber: req.body.suTestQuestionNumber},
-                          { $set: {suTestResult: 'correct'}});
-            }
-            else {
-              //suTestHistory
-              db.collection('suTestAnswerSheet')
-              .update({userID: req.body.userID,
-                       testID: req.body.testID,
-                       suTestNumber: req.body.suTestNumber,
-                       suTestID: req.body.suTestID,
-                       suTestMode: req.body.suTestMode,
-                       suTestStartAt: req.body.suTestStartAt,
-                       suTestQuestionNumber: req.body.suTestQuestionNumber},
-                          { $set: {suTestResult: 'wrong'}});
-            }
-          }
-        }
-        res.json('answer recorded');
-      }
-    }
-
-}
+ }
