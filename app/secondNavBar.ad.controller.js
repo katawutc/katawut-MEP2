7439511a06526fe9fa630f4fdbba38a537b46576
katawut.c <katawut.c@gmail.com>
2017-10-19 14:43:59 +0700

    add 2nd nav bar to admin page

---
+++ app/secondNavBar.ad.controller.js
@@ -0,0 +1,44 @@
+angular.module('app')
+.controller('secondNavBarAdCtrl',
+           ['$scope', '$window',
+            /*'suSecondNavBarMessageService',*/
+            'socketService',
+             secondNavBarAdCtrl]);
+
+function secondNavBarAdCtrl($scope, $window,
+                          /*  suSecondNavBarMessageService, */
+                            socketService) {
+
+  // shoe chat and note panels should ne $rootScope ?
+  $scope.showNotePanel = false;
+  $scope.showChatPanel = false;
+
+  $scope.openChatPanel = function() {
+
+    $scope.showChatPanel = !$scope.showChatPanel;
+  }
+
+  $scope.openNotePanel = function() {
+
+    $scope.showNotePanel = !$scope.showNotePanel;
+  }
+
+  // send chat message
+  $scope.sentMessage = [];
+
+  $scope.sendMessage = function() {
+      console.log('send chat message');
+      console.log($scope.message);
+
+
+      var message = {'userID': $window.sessionStorage.userID,
+                     'sentTime': Date.now(),
+                     'message': $scope.message}
+
+      socketService.emit('chat', message);
+
+      $scope.sentMessage.push($scope.message);
+
+      $scope.message = null;
+  }
+}
---
+++ app/secondNavBar.ad.directive.js
@@ -0,0 +1,8 @@
+// Navigation bar directive
+angular.module('app').directive('secondNavBarAd', secondNavbarAd);
+
+function secondNavbarAd() {
+  return {
+    templateUrl: 'secondNavBar.ad.html'
+  };
+}
