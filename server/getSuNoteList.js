060ff24ce51107c965a6dd9a9ecc21305e8895cd
katawut.c <katawut.c@gmail.com>
2017-11-07 08:08:33 +0700

    sort suNote in time reverse order

--- server/getSuNoteList.js
+++ server/getSuNoteList.js
@@ -4,21 +4,19 @@ module.exports = function getSuNoteList(req, res) {
   var mongo = require('./mongoDBConnect');
   var db = mongo.getDB();
 
-  console.log('at server: getSuNoteList: '+req.params.userID);
-
-  function cb(err, noteList) {
+  function suNote_cb(err, noteList) {
 
     if (err) throw err;
 
     if (noteList) {
 
-      console.log(noteList);
       res.json(noteList);
     }
   }
 
   db.collection('suNote')
   .find({userID: req.params.userID})
-  .toArray(cb);
+  .sort({$natural: -1})
+  .toArray(suNote_cb);
 
 }
