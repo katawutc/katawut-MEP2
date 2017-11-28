f2fd646ac06fed61aaa26209d7990c45269c5a63
katawut.c <katawut.c@gmail.com>
2017-11-28 12:38:36 +0700

    now know the previous page

--- app/loginHistory.admin.controller.js
+++ app/loginHistory.admin.controller.js
@@ -1,15 +1,29 @@
 angular.module('app')
 .controller('loginHistoryAdminCtrl',
-           ['$scope', '$log',
+           ['$scope',
             'loginHistoryAdmin', 'accountAdmin', 'loginHistoryCount',
              loginHistoryAdminCtrl]);
 
-function loginHistoryAdminCtrl($scope, $log,
+function loginHistoryAdminCtrl($scope,
                                loginHistoryAdmin, accountAdmin, loginHistoryCount) {
+
+  // last ID of the current page
+  var lastIDCurrentpage;
+  var previousPage;
+
+  $scope.maxSize = 5;
+  $scope.bigTotalItems = loginHistoryCount;
+  $scope.bigCurrentPage = 1;
+  previousPage = $scope.bigCurrentPage;
 
   if (loginHistoryAdmin !== null) {
 
     $scope.loginHistory = loginHistoryAdmin;
+    console.log(loginHistoryAdmin[loginHistoryAdmin.length-1]);
+
+    lastIDCurrentpage = loginHistoryAdmin[loginHistoryAdmin.length-1]._id;
+    console.log(lastIDCurrentpage);
+
   }
 
   if (accountAdmin !== null) {
@@ -20,24 +34,28 @@ function loginHistoryAdminCtrl($scope, $log,
   console.log(loginHistoryCount);
 
 
-  //$scope.totalItems = 64;
-  //$scope.currentPage = 4;
-
-  $scope.setPage = function (pageNo) {
-    $scope.currentPage = pageNo;
-  };
-
   $scope.pageChanged = function() {
 
+    console.log('previous page: '+ previousPage);
+
     console.log('Page changed to: ' + $scope.bigCurrentPage);
+
+    /**
+      * 1. if page change is next to the previous one
+      * 1a. if page is greater than the previous one
+      * 1b. if page is lesser than the previous one
+      * 2. if page change is not next to the previous one
+      */
+
+    console.log('Fetch data to display');
+
+    previousPage = $scope.bigCurrentPage;
+
+
+
+
   };
 
-  $scope.maxSize = 5;
-  $scope.bigTotalItems = loginHistoryCount;
-  $scope.bigCurrentPage = 1;
-
-  console.log($scope.bigCurrentPage);
-  console.log($scope.numPages);
 
 
 }
